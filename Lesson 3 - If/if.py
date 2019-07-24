39892465ee3c5ead548730dc9f3c0004ac27ff9b
Stanislav Hmelevsky <s.v.hmelevsky@gmail.com>
2019-07-24 15:50:20 +0400

    If-elif-else

---
+++ Lesson 3 - If/if.py
@@ -0,0 +1,10 @@
+# Exercise by if elif else conditions
+cars_list = ["Audi", "Opel", "Ford", "Bmw"]
+desired_car = "Bmw"
+
+if desired_car in cars_list:
+    print("Yes! This car has on the list")
+elif desired_car not in cars_list:
+    print("No! This car has no on the list")
+else:
+    print("This is are not are car!")
